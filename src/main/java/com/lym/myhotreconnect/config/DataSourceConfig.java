38cee9e1602fde30ee624e246548eec9e922cd9a
liuyumeng <308591952@qq.com>
2019-06-05 19:38:47 +0800

    恢复反例

--- src/main/java/com/lym/myhotreconnect/config/DataSourceConfig.java
+++ src/main/java/com/lym/myhotreconnect/config/DataSourceConfig.java
@@ -2,12 +2,9 @@ package com.lym.myhotreconnect.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.ctrip.framework.apollo.Config;
-import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
-import com.ctrip.framework.apollo.spring.annotation.ApolloConfigChangeListener;
 import com.lym.myhotreconnect.service.event.ApolloConfigChangeEvent;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -28,15 +25,17 @@ public class DataSourceConfig{
     private Config config;
 
     @Bean(MY_BEAN_DATA_SOURCE)
-    public DruidDataSource druidDataSource(){
-        return dataSource();
+    public DynamicDataSource dynamicDataSource(){
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        dynamicDataSource.setTargetDataSources(Collections.singletonMap(MY_DATA_SOURCE_KEY,dataSource()));
+        return dynamicDataSource;
     }
 
     /**
      * 数据库连接信息,从配置中心获取
      * @return
      */
-    private DruidDataSource dataSource(){
+    private DataSource dataSource(){
         DruidDataSource druidDataSource = new DruidDataSource();
         druidDataSource.setName(config.getProperty("data.source.name","dataSourceDefaultName"));
         druidDataSource.setUrl(config.getProperty("data.source.url","dataSourceDefaultUrl"));
@@ -50,13 +49,9 @@ public class DataSourceConfig{
     public void refreshDataSource(ApolloConfigChangeEvent apolloConfigChangeEvent){
         for (String key : apolloConfigChangeEvent.getConfigChangeEvent().changedKeys()) {
             if (key.startsWith("data.source.")) {
-                DruidDataSource druidDataSource = (DruidDataSource)applicationContext.getBean(MY_BEAN_DATA_SOURCE);
-                druidDataSource.setName(config.getProperty("data.source.name","dataSourceDefaultName"));
-                // 以下注释的参数不能重新设置
-                //druidDataSource.setUrl(config.getProperty("data.source.url","dataSourceDefaultUrl"));
-                //druidDataSource.setUsername(config.getProperty("data.source.username","dataSourceDefaultUsername"));
-                //druidDataSource.setPassword(config.getProperty("data.source.password",""));
-                druidDataSource.setDefaultReadOnly(Boolean.valueOf( config.getProperty("data.source.readonly","false")));
+                DataSourceConfig.DynamicDataSource dynamicDataSource = (DataSourceConfig.DynamicDataSource)applicationContext.getBean(MY_BEAN_DATA_SOURCE);
+                dynamicDataSource.setTargetDataSources(Collections.singletonMap(MY_DATA_SOURCE_KEY,dataSource()));
+                dynamicDataSource.afterPropertiesSet();
                 System.out.println("【数据库】动态切换成功...");
                 break;
             }
